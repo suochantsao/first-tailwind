d05226011389a4971074575eae67e6229ea4a2d9
suochantsao <suochantsao@gmail.com>
2021-09-28 17:28:02 +0800

    tailwind.config.js added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    // 在此參數中加入有使用到的檔案來做追蹤
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
