2f52a17c73613ff17342060e0ccf20f1e8c13485
suochantsao <suochantsao@gmail.com>
2021-10-20 20:30:07 +0800

    reset project settings / try first tailwind utilities

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import "./App.css";
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="App max-w-7xl border-2 rounded border-blue-100 my-10 mx-auto">
+      <header className="App-header m-7 ">
+        <h1 className="text-3xl mb-3">KEEP CALM and USE STACKOVERFLOW</h1>
         <a
-          className="App-link"
+          className="text-blue-600 border-2 border-blue-600 rounded p-2"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   purge: [
-    // 在此參數中加入有使用到的檔案來做追蹤
-    "./src/**/*.jsx",
+    // 將所有會使用到 Tailwind 的檔案路徑設定在這，讓 Tailwind 可以在生產環境建置時清除沒有使用的樣式
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
